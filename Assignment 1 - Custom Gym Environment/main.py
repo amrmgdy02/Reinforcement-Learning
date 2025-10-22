214c4ab9d3e1a3f21dd5f21916bdb21176a50f41
Amr Magdy <129008521+amrmgdy02@users.noreply.github.com>
2025-10-23 02:50:41 +0300

    all possible envs code

--- Assignment 1 - Custom Gym Environment/main.py
+++ Assignment 1 - Custom Gym Environment/main.py
@@ -3,6 +3,29 @@ from grid_maze_env import GridMazeEnv
 from policy_iterator import PolicyIterator
 from gymnasium.wrappers import RecordVideo
 
+def generate_all_possible_envs(size=5, num_mines=2) -> list:
+    """generate all possible environments (all possible mines positions and target positions)
+
+    Returns:
+        list: A list of all possible GridMazeEnv instances.
+    """
+    
+    envs = []
+    size = 5
+    num_mines = 2
+    all_positions = [(x, y) for x in range(size) for y in range(size)]
+    
+    for target_pos in all_positions:
+        remaining_positions = [pos for pos in all_positions if pos != target_pos]
+        
+        for i in range(len(remaining_positions)):
+            for j in range(i + 1, len(remaining_positions)):
+                mine_positions = [np.array(remaining_positions[i]), np.array(remaining_positions[j])]
+                env = GridMazeEnv(size=size, mines=np.array(mine_positions), target_location=target_pos,render_mode="rgb_array")
+                envs.append(env)
+    
+    return envs
+
 def get_agent_success_rate(env: GridMazeEnv, gamma: float, theta: float, num_episodes: int = 100) -> float:
         """ create agent for num_episodes environments and let them play the game.
 
@@ -63,11 +86,11 @@ def get_diff_agents_success_rate(gamma: float, theta: float, num_episodes=100):
 
 def main():
     
-    custom_env = GridMazeEnv(size=10, layout_seed=33, render_mode="rgb_array")
+    custom_env = GridMazeEnv(size=5, num_mines=2, render_mode="rgb_array")
     
     # custom_env.save_env_as_image("imgs/grid_maze_env_1.png")
     
-    pi = PolicyIterator(custom_env, gamma=0.7)
+    pi = PolicyIterator(custom_env, gamma=0.99, theta=1e-6)
     optimal_policy, optimal_value_function, num_iterations = pi.iterate()
     
     print(f"Policy Iteration converged in {num_iterations} iterations.")
@@ -88,8 +111,17 @@ def main():
     video_env.close()
 
 if __name__ == "__main__":
-    main()
-    
+    #main()
+    # envs = generate_all_possible_envs(size=5, num_mines=2)
+    # optimal_policies = {}
+    # for i, env in enumerate(envs):
+    #     pi = PolicyIterator(env, gamma=0.99)
+    #     optimal_policy, optimal_value_function, num_iterations = pi.iterate()
+    #     optimal_policies[(tuple(env._target_location), tuple(map(tuple, env._mines)))] = optimal_policy
+    #     print(f"Processed environment {i + 1}/{len(envs)}")
+
+    # # save optimal_policies to a file if needed
+    # np.save("optimal_policies.npy", optimal_policies)
     #success_rate = get_agent_success_rate(GridMazeEnv(size=5, layout_seed=33, render_mode="rgb_array"), gamma=0.99, theta=1e-3, num_episodes=100)
     #print(f"Agent Success Rate over 100 episodes: {success_rate * 100:.2f}%")
     
