225da8af4afdbc6f248b660c21340b75c0275bbf
Amr Magdy <129008521+amrmgdy02@users.noreply.github.com>
2025-12-11 05:48:22 +0200

    updated td3

--- Assignment 4 - SOTA model-free algorithms/TD3.py
+++ Assignment 4 - SOTA model-free algorithms/TD3.py
@@ -1,3 +1,4 @@
+from cmath import tau
 import numpy as np
 import torch
 import torch.nn as nn
@@ -5,6 +6,7 @@ import torch.nn.functional as F
 import copy
 import gymnasium as gym
 import wandb
+import os
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -171,7 +173,7 @@ class TD3(object):
     def train(
         self, env, replay_buffer, total_timesteps,
         batch_size=256, steps_to_log=10000,
-        start_timesteps=20000, exploration_noise=0.1, noise_label="0.1"
+        start_timesteps=20000, exploration_noise=0.2
     ):
         state, _ = env.reset()
         state = flatten_state(state)
@@ -184,6 +186,12 @@ class TD3(object):
         s = flatten_state(s)
 
         for t in range(total_timesteps):
+            
+            # noise_start = exploration_noise 
+            # noise_end = 0.05
+            # decay_ratio = max(0, 1 - t / total_timesteps) # Decay linearly over all timesteps
+            # current_exploration_noise = noise_end + (noise_start - noise_end) * decay_ratio
+            
             if replay_buffer.size < start_timesteps:
                 action = env.action_space.sample()
             else:
@@ -204,55 +212,71 @@ class TD3(object):
 
             if done or truncated:
                 print(
-                    f"Noise {noise_label} | Step: {t+1} | Episode: {len(episodes_rewards)+1} | "
+                    f"Step: {t+1} | Episode: {len(episodes_rewards)+1} | "
                     f"Reward: {episode_reward:.1f} | Duration: {episode_duration}"
                 )
                 episodes_rewards.append(episode_reward)
                 
-                # Log to W&B with noise identifier
-                wandb.log({
-                    f"episode_reward_exploration_noise_{noise_label}": episode_reward,
-                    f"episode_duration_exploration_noise_{noise_label}": episode_duration,
-                    "timestep": t + 1
-                })
+                episode_idx = len(episodes_rewards)  # next episode index (1-based)
+                wandb.log({"training_episode_reward": episode_reward}, step=episode_idx)
                 
                 episode_reward = 0
                 episode_duration = 0
                 state, _ = env.reset()
                 state = flatten_state(state)
+                
+                # if std of last 20 episodes is < 20, we can consider stopping early
+                # if len(episodes_rewards) >= 20 and np.std(episodes_rewards[-20:]) < 20:
+                #     print(f"Early stopping at timestep {t+1} due to satbilizing (STD of last 20 episodes < 20).")
+                #     break
 
             if (t + 1) % steps_to_log == 0:
                 avg = np.mean(episodes_rewards[-10:]) if len(episodes_rewards) >= 10 else np.mean(episodes_rewards)
-                print(f"Noise {noise_label} | [{t+1}] Average reward (last 10 episodes): {avg:.2f}")
+                print(f"[{t+1}] Average reward (last 10 episodes): {avg:.2f}")
+                print(f"STD of reward (last 10 episodes): {np.std(episodes_rewards[-10:]):.2f}")
                 
                 # Log average reward to W&B with noise identifier
                 wandb.log({
-                    f"avg_reward_last_10_noise_{noise_label}": avg,
+                    f"avg_reward_last_10": avg,
+                    f"std_reward_over_10_episodes": np.std(episodes_rewards[-10:]),
                     "timestep": t + 1
                 })
+                
+    def save(self, filename):
+        torch.save(self.actor.state_dict(), filename + "_actor.pth")
+        torch.save(self.critic_1.state_dict(), filename + "_critic_1.pth")
+        torch.save(self.critic_2.state_dict(), filename + "_critic_2.pth")
+        
+    def load(self, filename):
+        self.actor.load_state_dict(torch.load(filename + "_actor.pth"))
+        self.critic_1.load_state_dict(torch.load(filename + "_critic_1.pth"))
+        self.critic_2.load_state_dict(torch.load(filename + "_critic_2.pth"))
+        
+        self.actor_target = copy.deepcopy(self.actor)
+        self.critic_1_target = copy.deepcopy(self.critic_1)
+        self.critic_2_target = copy.deepcopy(self.critic_2)
 
 
 if __name__ == "__main__":
     env_name = "LunarLander-v3"
-    exploration_noises = [0.05 , 0.1, 0.3, 0.5]
     
     wandb.login()
     
     run = wandb.init(
-        project="TD3-LunarLander-v3-Exploration-Noise-Tuning",
-        name=f"TD3_{env_name}_all_noises",
+        project="TD3-LunarLander-v3-final",
+        name=f"TD3_{env_name}_Train_Eval",
         config={
             "environment": env_name,
             "algorithm": "TD3",
-            "exploration_noises": exploration_noises,
-            "total_timesteps": 50_000,
+            "exploration_noises": 0.2,
+            "total_timesteps": 200_000,
             "start_timesteps": 10_000,
             "batch_size": 256,
             "discount": 0.99,
             "tau": 0.005,
             "policy_noise": 0.2,
             "noise_clip": 0.5,
-            "policy_freq": 2,
+            "policy_freq": 5,
             "actor_lr": 3e-4,
             "critic_lr": 3e-4
         }
@@ -264,30 +288,89 @@ if __name__ == "__main__":
     action_dim = env.action_space.shape[0]
     max_action = float(env.action_space.high[0])
     
-    for noise in exploration_noises:
-        print(f"\n{'='*60}")
-        print(f"Training with exploration noise: {noise}")
-        print(f"{'='*60}\n")
+    print(f"\n{'='*60}")
+    print(f"Training with configurations:", {
+        "environment": env_name,
+        "algorithm": "TD3",
+        "exploration_noises": 0.2,
+        "total_timesteps": 250_000,
+        "start_timesteps": 10_000,
+        "batch_size": 256,
+        "discount": 0.99,
+        "tau": 0.005,
+        "policy_noise": 0.2,
+        "noise_clip": 0.5,
+        "policy_freq": 5,
+        "actor_lr": 3e-4,
+        "critic_lr": 3e-4
+    })
+    print(f"{'='*60}\n")
         
-        agent = TD3(flat_dim, action_dim, max_action)
+    agent = TD3(flat_dim, 
+                action_dim, 
+                max_action,
+                tau=0.005,
+                policy_freq=5
+                )
         
-        if env_name == "CarRacing-v3":
-            buffer = ReplayBuffer(flat_dim, action_dim, max_size=1_000)
-        else:
-            buffer = ReplayBuffer(flat_dim, action_dim)
+    if env_name == "CarRacing-v3":
+        buffer = ReplayBuffer(flat_dim, action_dim, max_size=1_000)
+    else:
+        buffer = ReplayBuffer(flat_dim, action_dim)
 
-        agent.train(
-            env,
-            buffer,
-            total_timesteps=30_000,
-            start_timesteps=5_000,
-            exploration_noise=noise,
-            noise_label=str(noise)
-        )
+    agent.train(
+        env,
+        buffer,
+        total_timesteps=300_000,
+        start_timesteps=10_000,
+        exploration_noise=0.1,
+    )
+        
+    # eval on 100 episodes and log results (mean and std) to wandb and also plot rewards with episodes
+    eval_episodes = 100
+    eval_rewards = []
+    for ep in range(eval_episodes):
+        state, _ = env.reset()
+        state = flatten_state(state)
+        done = False
+        truncated = False
+        ep_reward = 0
+        while not (done or truncated):
+            action = agent.select_action(np.array(state))
+            next_state, reward, done, truncated, _ = env.step(action)
+            next_state = flatten_state(next_state)
+            ep_reward += reward
+            state = next_state
+        eval_rewards.append(ep_reward)
+        print(f"Evaluation Episode {ep+1}: Reward: {ep_reward:.2f}")
+        
+        # Log each evaluation episode reward to W&B
+        wandb.log({
+            f"eval_episode_reward": ep_reward
+        })
+            
+    avg_eval_reward = np.mean(eval_rewards)
+    print(f"Average Evaluation Reward over {eval_episodes} episodes: {avg_eval_reward:.2f}")
+    print(f"STD Evaluation over {eval_episodes} episodes: {np.std(eval_rewards):.2f}")
+    std = np.std(eval_rewards)
+    wandb.log({
+        f"eval_avg_reward": avg_eval_reward,
+        f"eval_std_reward": std
+    })
+    
+    
+    # Save the agent
+    os.makedirs("TD3-models", exist_ok=True)
+    agent.save(f"TD3-models/td3_final_{env_name}")
+    
+    # Save the configs
+    with open(f"TD3-models/td3_final_{env_name}_config.txt", "w") as f:
+        for key, value in run.config.items():
+            f.write(f"{key}: {value}\n")
 
     env.close()
     wandb.finish()
-    
+        
     print(f"\n{'='*60}")
     print("All experiments completed!")
     print(f"{'='*60}")
